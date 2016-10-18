27e2b018fb0832b659be1a8de28328258970387b
Eyaz Rehman <eyaz.rehman@indigogames.ca>
2016-10-18 00:17:33 -0400

    Added support for original read/write callbacks.

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Example curldump.ini:
 ```
 [CURL]
 SetOpt=55 8B EC 83 EC 0C 8B 45 0C 53 33 D2 56 57 89 55 FC 3D 11 27 00 00
-Close=
-HookDelay=5000
+Close=55 8B EC 56 8B 75 08 57 33 FF 3B F7 0F 84 ?? ?? ?? ?? 57 56 E8
+HookDelay=10000
 ```
--- Source/DllMain.cpp
+++ Source/DllMain.cpp
@@ -29,60 +29,118 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+typedef size_t (*__cdecl CurlIOCallback)(char *data, size_t size, size_t bytes, void *userdata);
+
+struct CurlInstance {
+	void *Handle;
+	void *WriteData;
+	void *ReadData;
+	CurlIOCallback WriteCallback;
+	CurlIOCallback ReadCallback;
+};
+
 indigo::ACPDump acp_dump_;
-std::map<void *, bool> redirect_set_;
+std::map<void *, CurlInstance *> redirect_set_;
 std::mutex redirect_mutex_;
 indigo::CallHook curl_setopt_hook_;
 indigo::CallHook curl_close_hook_;
 
-size_t curl_write_callback(char *data, size_t size, size_t bytes, void *userdata) {
+size_t curl_write_callback(char *data, size_t size, size_t bytes, CurlInstance *instance) {
 	// actual size == size * bytes
-	printf("CurlDump: Writing %d bytes\n", size);
+	printf("CurlDump: (0x%08p) Writing %d bytes\n", instance->Handle, size * bytes);
 	acp_dump_.Write(SOCK_STREAM, IPPROTO_TCP, static_cast<uint32_t>(inet_addr("127.0.0.1")),
-		reinterpret_cast<uint16_t>(userdata), reinterpret_cast<uint32_t>(userdata), 1337, data, size * bytes);
+		reinterpret_cast<uint16_t>(instance->Handle), reinterpret_cast<uint32_t>(instance->Handle), 1337, data, size * bytes);
 
-	return size * bytes;
+	return instance->WriteCallback != nullptr ? instance->WriteCallback(data, size, bytes, instance->WriteData) : 0;
 }
 
-size_t curl_read_callback(char *data, size_t size, size_t bytes, void *userdata) {
+size_t curl_read_callback(char *data, size_t size, size_t bytes, CurlInstance *instance) {
 	// actual size == size * bytes
-	printf("CurlDump: Reading %d bytes\n", size);
-	acp_dump_.Write(SOCK_STREAM, IPPROTO_TCP, reinterpret_cast<uint32_t>(userdata), 1337, 
-		static_cast<uint32_t>(inet_addr("127.0.0.1")), reinterpret_cast<uint16_t>(userdata), data, size * bytes);
+	printf("CurlDump: (0x%08p) Reading %d bytes\n", instance->Handle, size * bytes);
+	acp_dump_.Write(SOCK_STREAM, IPPROTO_TCP, reinterpret_cast<uint32_t>(instance->Handle), 1337,
+		static_cast<uint32_t>(inet_addr("127.0.0.1")), reinterpret_cast<uint16_t>(instance->Handle), data, size * bytes);
 
-	return size * bytes;
+	return instance->ReadCallback != nullptr ? instance->ReadCallback(data, size, bytes, instance->ReadData) : 0;
 }
 
-// 0x0138F740 int __cdecl Curl_setopt(void *handle, signed int option, unsigned int *param)
-int __cdecl curl_setopt_(void *handle, signed int option, unsigned int *param) {
+// int __cdecl Curl_setopt(void *handle, signed int option, va_list param)
+int __cdecl curl_setopt_(void *handle, signed int option, va_list param) {
+	printf("CurlDump: Curl_setopt(0x%08p, %d, 0x%08p)\n", handle, option, param);
+
+	// Curl instance
+	CurlInstance *instance;
+
 	// Get the original
-	auto curl_setopt = curl_setopt_hook_.Get<int(*__cdecl)(void *, signed int, unsigned int *)>();
+	auto curl_setopt = curl_setopt_hook_.Get<int(*__cdecl)(void *, signed int, va_list)>();
 
 	redirect_mutex_.lock();
 	if (redirect_set_.find(handle) == redirect_set_.end()) {
 		// Initialize
-		printf("CurlDump: Redirecting handle 0x%08p\n", handle);
+		printf("CurlDump: Monitoring easy handle 0x%08p\n", handle);
 
-		// TODO/NOTE: curl_setopt takes a variadic argument, see curl_easy_setopt
+		// Create curl instance
+		instance = new CurlInstance{ nullptr };
+		instance->Handle = handle;
 
-		curl_setopt(handle, CURLOPT_VERBOSE, reinterpret_cast<unsigned int *>(1));
-		curl_setopt(handle, CURLOPT_WRITEDATA, static_cast<unsigned int *>(handle));
-		curl_setopt(handle, CURLOPT_READDATA, static_cast<unsigned int *>(handle));
-		curl_setopt(handle, CURLOPT_WRITEFUNCTION, reinterpret_cast<unsigned int *>(&curl_write_callback));
-		curl_setopt(handle, CURLOPT_READFUNCTION, reinterpret_cast<unsigned int *>(&curl_read_callback));
+		if (option == CURLOPT_WRITEDATA) {
+			instance->WriteData = va_arg(param, void *);
+		} else if (option == CURLOPT_READDATA) {
+			instance->ReadData = va_arg(param, void *);
+		} else if (option == CURLOPT_WRITEFUNCTION) {
+			instance->WriteCallback = va_arg(param, CurlIOCallback);
+		} else if (option == CURLOPT_READFUNCTION) {
+			instance->ReadCallback = va_arg(param, CurlIOCallback);
+		}
 
-		redirect_set_[handle] = true;
+		auto setopt = [=](CURLoption opt, ...) {
+			printf("CurlDump: Setting option %d for easy handle 0x%08p\n", opt, handle);
+
+			va_list va;
+			va_start(va, opt);
+
+			curl_setopt(handle, opt, va);
+
+			va_end(va);
+		};
+
+		setopt(CURLOPT_VERBOSE, 1);
+		setopt(CURLOPT_WRITEDATA, instance);
+		setopt(CURLOPT_READDATA, instance);
+		setopt(CURLOPT_WRITEFUNCTION, &curl_write_callback);
+		setopt(CURLOPT_READFUNCTION, &curl_read_callback);
+
+		redirect_set_[handle] = instance;
 	}
 
 	redirect_mutex_.unlock();
+
+	instance = redirect_set_[handle];
+	if (option == CURLOPT_WRITEDATA) {
+		instance->WriteData = va_arg(param, void *);
+		return 0;
+	} 
+	if (option == CURLOPT_READDATA) {
+		instance->ReadData = va_arg(param, void *);
+		return 0;
+	} 
+	if (option == CURLOPT_WRITEFUNCTION) {
+		instance->WriteCallback = va_arg(param, CurlIOCallback);
+		return 0;
+	} 
+	if (option == CURLOPT_READFUNCTION) {
+		instance->ReadCallback = va_arg(param, CurlIOCallback);
+		return 0;
+	}
+
 	return curl_setopt(handle, option, param);
 }
 
-// 0x0138F330 int __cdecl Curl_close(void *handle)
+// int __cdecl Curl_close(void *handle)
 int __cdecl curl_close_(void *handle) {
 	redirect_mutex_.lock();
 	for (auto it = redirect_set_.begin(); it != redirect_set_.end();) {
 		if (it->first == handle) {
+			delete it->second;
 			it = redirect_set_.erase(it);
 		} else {
 			++it;
@@ -95,6 +153,14 @@ int __cdecl curl_close_(void *handle) {
 
 extern "C" {
 	EXPORT_ATTR void __cdecl onExtensionUnloading(void) {
+		// Remove curl instances
+		redirect_mutex_.lock();
+		for (auto it = redirect_set_.begin(); it != redirect_set_.end();) {
+			delete it->second;
+			it = redirect_set_.erase(it);
+		}
+		redirect_mutex_.unlock();
+
 		// Close dump
 		acp_dump_.Close();
 
@@ -142,6 +208,11 @@ extern "C" {
 			return;
 		}
 
+		if (close.empty()) {
+			printf("CurlDump: Invalid Curl_close pattern\n");
+			return;
+		}
+
 		// Install hooks
 		std::thread([=]() {
 			std::this_thread::sleep_for(std::chrono::milliseconds(delay));
@@ -154,12 +225,9 @@ extern "C" {
 					continue;
 				}
 
-				// "Optional"
-				if (!close.empty()) {
-					if (!curl_close_hook_.Install(indigo::Memory::Find(close.c_str()).Get(0).Get<void *>(), &curl_close_)) {
-						printf("CurlDump: Failed to install Curl_close hook\n");
-						continue;
-					}
+				if (!curl_close_hook_.Install(indigo::Memory::Find(close.c_str()).Get(0).Get<void *>(), &curl_close_)) {
+					printf("CurlDump: Failed to install Curl_close hook\n");
+					continue;
 				}
 
 				// Open dump
